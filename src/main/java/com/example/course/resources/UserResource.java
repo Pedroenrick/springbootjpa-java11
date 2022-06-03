0dd8b3b81b1219022001657cf5c7e80025fc8fe8
Pedroenrick Felizardo <Pedro_penna22@hotmail.com>
2022-06-03 10:30:54 -0300

    Entity User, resource, H2 database, teste profile, JPA

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.example.course.resources;
+
+import com.example.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Pedro", "Pedro_penna22@hotmail.com", "999999999", "123456");
+        return ResponseEntity.ok().body(user);
+    }
+}
